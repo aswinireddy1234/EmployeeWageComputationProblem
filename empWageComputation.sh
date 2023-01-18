74d89691dc25971526ed026a972c88b4c31a3192
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-01-18 20:06:01 +0530

    Add: Calculate Daily Wage - Assume Wage Per Hour is 20 - Assume Full Day Hour is 8

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,6 +6,11 @@ randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
 then
 echo "Employee is present"
+empRatePerHr=20;
+empHRS=8;
+salary=$(($empHrs*$empRatePerHr))
+echo "salary="$salary
 else
 echo "Employee is absent"
+echo "salary="0
 fi
