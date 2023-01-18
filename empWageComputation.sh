b8c4476ab725b63d3af8d83d5837155e2a7c662a
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-01-18 21:35:27 +0530

    Add: Calculate Wage For a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,25 @@ echo "Welcome to employee wage computation problem"
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
+numWorkingDays=20;
+
+for (( day=1; day<=numWorkingDays; day++ ))
+do
 randomCheck=$((RANDOM%3));
 
 case $randomCheck in $isFullTime)
-   empHrs=8;;
-   $isPartTime)
-   empHrs=4;;
-   *)
-   empHrs=0;;
-esac
-salary=$(($empHrs*$empRatePerHr))
-echo "salary=$salary"
 
+                      empHrs=8;;
+$isPartTime)
+                       empHrs=4;;
+                         *)
+                        empHrs=0;;
+ esac
+
+salary=$(($empHrs*$empRatePerHr));
+totalSalary=$(($totalSalary+$salary))
+done
+echo "Employee has earned $totalSalary$ in a month";
 
