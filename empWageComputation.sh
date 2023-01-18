bdf559b21d49688ad4491d252179274ddb527b79
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-01-18 23:25:36 +0530

    Refector: refactor the code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,4 @@
-echo "Welcome to employee wage computation problem"
+echo "welcome to Employee wage computation program"
 isPartTime=1;
 isFullTime=2;
 totalSalary=0;
@@ -7,25 +7,34 @@ totalWorkingHour=20;
 empRatePerHr=20;
 numWorkingDays=20;
 day=1;
+
+function calculateWorkingHour() {
+       case $randomCheck in $siFullTime)
+       empHrs=8;;
+       $isPartTime)
+       empHrs=4;;
+       *)
+        empHrs=0;;
+esac
+echo $empHrs;
+}
 while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
 randomCheck=$((RANDOM%3));
-case $randomCheck in $isFullTime)
-empHrs=8;;
-$isPartTime)
-empHrs=4;;
-*)
-empHrs=0;;
-esac
-totalWorkingHour=$(($totalWorkingHour+$empHrs))
+
+    wHour=$(calculateWorkingHour $randomCheck);
+   totalWorkingHour=$(($totalWorkingHour + $wHour));
 if [ $totalWorkingHour -gt 100 ]
+
 then
-totalWorkingHour=$(($totalWorkingHour-$empHrs))
+totalWorkingHour=$(($totalWorkingHour-$wHour));
 break;
 fi
-salary=$(($empRatePerHr*$empHrs))
+salary=$(($empRatePerHr*$wHour));
+
 totalSalary=$(($totalSalary+$salary))
 ((day++))
 done
-echo "Employee has earned $totalSalary$ in a month (Total
-working Hour :$totalWorkingHour)";
+echo "Employee has earned $totalSalary$ in a month (Total working Hour :$totalWorkingHour)";
+
+
